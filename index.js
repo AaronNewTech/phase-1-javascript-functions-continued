609c310f7fd1d86b0849d785e821a9b811331dc9
Aaron <arnhsmith@gmail.com>
2023-06-13 07:45:20 -0400

    finished and checked code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+
+function saturdayFun(text) {
+
+    if (text === undefined){
+        text = 'roller-skate'
+    }
+    return `This Saturday, I want to ${text}!`
+}
+
+function mondayWork(text) {
+
+    if (text === undefined) {
+        text = 'go to the office'
+    }
+    return `This Monday, I will ${text}.`
+}
+
+function wrapAdjective(dec) {
+    console.log(dec)
+    
+
+    const innerFunction = function(string) {
+            console.log(string)
+
+        if (string === undefined) {
+            string = "special"
+        }
+        console.log(string)
+        return `You are ${dec}${string}${dec}!`
+
+    }
+    return innerFunction
+
+
+}
+
+wrapAdjective(dec)(emphatic)
